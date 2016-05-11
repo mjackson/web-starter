e458c57081fa562b3ed0a4f0f66a37208a511de8
Michael Jackson <mjijackson@gmail.com>
2016-05-11 12:40:31 -0700

    Add correct doctype

--- modules/server/RenderUtils.js
+++ modules/server/RenderUtils.js
@@ -2,5 +2,7 @@ import React from 'react'
 import { renderToStaticMarkup } from 'react-dom/server'
 import HomePage from './components/HomePage'
 
+const doctype = '<!doctype html>'
+
 export const renderHomePage = (props = {}) =>
-  renderToStaticMarkup(<HomePage {...props}/>)
+  doctype + renderToStaticMarkup(<HomePage {...props}/>)
