984e5a0ef63f1268a559780cd492722f941d48dd
Michael Jackson <mjijackson@gmail.com>
2016-06-21 18:24:43 -0700

    Use parseInt so we have integers

--- modules/server/ServerConfig.js
+++ modules/server/ServerConfig.js
@@ -1,10 +1,10 @@
 import path from 'path'
 
 export const id = 1
-export const port = process.env.PORT || 5000
+export const port = parseInt(process.env.PORT, 10) || 5000
 export const webpackConfig = require('../../webpack.config')
 export const statsFile = path.resolve(__dirname, '../../stats.json')
 export const publicDir = path.resolve(__dirname, '../../public')
-export const timeout = 20000
+export const timeout = parseInt(process.env.TIMEOUT, 10) || 20000
 export const sessionDomain = process.env.SESSION_DOMAIN
 export const sessionSecret = process.env.NODE_ENV === 'development' ? 'development' : process.env.SESSION_SECRET
